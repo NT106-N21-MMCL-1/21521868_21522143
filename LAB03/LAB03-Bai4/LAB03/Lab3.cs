18915db1c4270fbd038b9902166075388cb2df9b
BaoPC <97841708+BaoPC@users.noreply.github.com>
2023-05-06 17:45:31 +0700

    Bài 4 fix lỗi

---
+++ LAB03/LAB03-Bai4/LAB03/Lab3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB03
+{
+    public partial class Lab03 : Form
+    {
+        public static bool isExistServer = false;
+        public Lab03()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Server server = new Server();
+        }
+    }
+}
