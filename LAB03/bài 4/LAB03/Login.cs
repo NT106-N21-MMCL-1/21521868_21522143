066a384616c9b655a8c5ff6ff9eacbaaf1b2a152
BaoPC <97841708+BaoPC@users.noreply.github.com>
2023-05-10 12:58:48 +0700

    fix lỗi

---
+++ LAB03/bài 4/LAB03/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB03
+{
+    public partial class Login : Form
+    {
+
+        public static string strName { get; set; }
+
+        public static string strIP { get; set; }
+        public Login()
+        {
+            InitializeComponent();
+            this.Show();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            strName = txtName.Text;
+            strIP = textBox1.Text;
+
+            Client client = new Client(strName, strIP);
+            client.Text = strName;
+            this.Close();
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            if (txtName.Text == string.Empty)
+                buttonOK.Enabled = false;
+            else 
+                buttonOK.Enabled = true;
+        }
+    }
+}
---
+++ LAB03/bài 4/LAB03/Server.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace LAB03
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstChatters = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lstChatters
+            // 
+            this.lstChatters.FormattingEnabled = true;
+            this.lstChatters.ItemHeight = 16;
+            this.lstChatters.Location = new System.Drawing.Point(967, 12);
+            this.lstChatters.Name = "lstChatters";
+            this.lstChatters.Size = new System.Drawing.Size(154, 548);
+            this.lstChatters.TabIndex = 19;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(949, 545);
+            this.listView1.TabIndex = 20;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 568);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lstChatters);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Server_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstChatters;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
