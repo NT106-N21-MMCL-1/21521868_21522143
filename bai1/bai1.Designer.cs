0b59c5b70653de6aae8cd76db75e71aa8ca1099c
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2023-06-01 17:04:32 +0700

    add bai1 commit.

---
+++ bai1/bai1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace bai1
+{
+    partial class bai1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtsender = new System.Windows.Forms.TextBox();
+            this.txtrec = new System.Windows.Forms.TextBox();
+            this.txtsub = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.rtxt = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtsender
+            // 
+            this.txtsender.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtsender.Location = new System.Drawing.Point(268, 76);
+            this.txtsender.Name = "txtsender";
+            this.txtsender.Size = new System.Drawing.Size(388, 27);
+            this.txtsender.TabIndex = 0;
+            // 
+            // txtrec
+            // 
+            this.txtrec.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtrec.Location = new System.Drawing.Point(267, 110);
+            this.txtrec.Name = "txtrec";
+            this.txtrec.Size = new System.Drawing.Size(389, 27);
+            this.txtrec.TabIndex = 1;
+            // 
+            // txtsub
+            // 
+            this.txtsub.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtsub.Location = new System.Drawing.Point(149, 163);
+            this.txtsub.Name = "txtsub";
+            this.txtsub.Size = new System.Drawing.Size(687, 27);
+            this.txtsub.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(153, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sender :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(153, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Recipient :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(30, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Subject :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(29, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Content";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnsend
+            // 
+            this.btnsend.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnsend.Location = new System.Drawing.Point(29, 78);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(77, 35);
+            this.btnsend.TabIndex = 8;
+            this.btnsend.Text = "SEND";
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtxt
+            // 
+            this.rtxt.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rtxt.Location = new System.Drawing.Point(149, 203);
+            this.rtxt.Name = "rtxt";
+            this.rtxt.Size = new System.Drawing.Size(684, 300);
+            this.rtxt.TabIndex = 9;
+            this.rtxt.Text = "";
+            // 
+            // bai1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 619);
+            this.Controls.Add(this.rtxt);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsub);
+            this.Controls.Add(this.txtrec);
+            this.Controls.Add(this.txtsender);
+            this.Name = "bai1";
+            this.Text = "bai1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtsender;
+        private System.Windows.Forms.TextBox txtrec;
+        private System.Windows.Forms.TextBox txtsub;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnsend;
+        private System.Windows.Forms.RichTextBox rtxt;
+    }
+}
+
